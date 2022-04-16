7cf8060b22a50abb92a8882cb59ca7d7b443a6bb
igorek <igor.medvedev.91@internet.ru>
2022-04-16 21:58:29 +0300

    temperatureHome

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tempeturaCelc = 30;
+
+        double tempFarengate = tempeturaCelc * 1.8 + 32 ;
+
+        double temperatureKel = 30 + 273.15;
+
+
+        System.out.println( "Температура по Кельвину = " + temperatureKel);
+        System.out.println( "Температура по Фаренгейту = " + tempFarengate);
+
+
+    }
+}
